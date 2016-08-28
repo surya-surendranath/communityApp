f18eba85719f9cf757a72d01b39e481d25b1c73e
surya-surendranath <surya.snath08@gmail.com>
2016-08-28 17:09:16 +1200

    CRUD operation Delete Read Update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express=require('express');
 var app = express();
 var fs = require('fs');
+
 user = {
    "user4" : {
       "name" : "mohit",
@@ -9,13 +10,35 @@ user = {
       "id": 4
    }
 }
-app.get('/:id', function(req, res) {
-	fs.readFile(__dirname +"/" + "users.json", 'utf8', function(err, data){
-		users =JSON.parse(data);
-		var user =users["user" + req.params.id]
-		res.end(JSON.stringify(user));
-	})
+
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      users = JSON.parse( data );
+      var user = users["user" + req.params.id] 
+      console.log( user );
+      res.end( JSON.stringify(user));
+   });
 })
+app.get('/deleteUser', function(req, res){
+	fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+		data= JSON.parse(data);
+		delete data["user" + 2];
+		res.end(JSON.stringify(data));
+	});
+})
+
+app.get('/updateUser', function(req, res){
+	fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+		data= JSON.parse(data);
+
+		console.log(data)
+		data.user1.name="mohini";
+		
+		res.end(JSON.stringify(data));
+	});
+})
+
 app.get('/listUsers', function(req, res) {
 	fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data) {
 		console.log(data);
@@ -28,7 +51,7 @@ app.get('/addUser', function(req, res) {
 		data["user4"] = user["user4"];
 		console.log(data);
 		res.end(JSON.stringify(data));
-	})
+	});
 })
 
 	server =app.listen(8081, function() {
